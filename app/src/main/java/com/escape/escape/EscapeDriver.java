d39c2ff15e6330756ac8a3cfcb326ddb75b75aef
Adam Holt <Adam Holt>
2015-12-04 12:31:35 -0700

    Separated methods in EscapeDriver to make extra levels easier to create.

--- app/src/main/java/com/escape/escape/EscapeDriver.java
+++ app/src/main/java/com/escape/escape/EscapeDriver.java
@@ -15,17 +15,12 @@ public class EscapeDriver
 	public EscapeDriver()
 	{
 		gameObjects = new ArrayList<>();
-		buildGameObjects();
+		buildLevel1();
 		GameBoard.getGameBoard(gameObjects);
 	}
 
-	public void buildGameObjects()
+	private void buildGameBoard(String[] objectModels, int[] xCoords, int[] yCoords)
 	{
-		//List of GameObjects to be placed on the board
-		String[] objectModels = {"player","obstacle","obstacle","obstacle","obstacle","obstacle","obstacle","enemy","enemy","enemy","exit"};
-		int[] xCoords = {1,2,4,5,6,6,7,2,3,8,7};
-		int[] yCoords = {8,1,3,8,8,1,1,2,5,3,3};
-
 		for(int i=0; i<objectModels.length; i++) {
 			try {
 				gameObjects.add(GameObjectFactory.getGameObject(objectModels[i], xCoords[i], yCoords[i]));
@@ -33,5 +28,14 @@ public class EscapeDriver
 				e.printStackTrace();
 			}
 		}
+	}
+
+	private void buildLevel1()
+	{
+		//List of GameObjects to be placed on the board
+		String[] objectModels = {"player","obstacle","obstacle","obstacle","obstacle","obstacle","obstacle","enemy","enemy","enemy","exit"};
+		int[] xCoords = {1,2,4,5,6,6,7,2,3,8,7};
+		int[] yCoords = {8,1,3,8,8,1,1,2,5,3,3};
+		buildGameBoard(objectModels, xCoords, yCoords);
 	}
 }
