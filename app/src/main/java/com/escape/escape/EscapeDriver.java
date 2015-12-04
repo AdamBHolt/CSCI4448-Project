1a9894092fd490ce142fadae7f2844bd7984b0cc
Adam Holt <Adam Holt>
2015-12-04 11:58:26 -0700

    Added exception handling and simplified fetching of GameObjects

--- app/src/main/java/com/escape/escape/EscapeDriver.java
+++ app/src/main/java/com/escape/escape/EscapeDriver.java
@@ -1,29 +1,37 @@
 package com.escape.escape;
+
 import java.util.ArrayList;
 
 public class EscapeDriver
 {
+	//List of GameObjects
 	private ArrayList<GameObject> gameObjects;
-	public static void main(String[] args){
+
+	public static void main(String[] args)
+	{
 		new EscapeDriver();
-	}	
-	public EscapeDriver(){
+	}
+
+	public EscapeDriver()
+	{
 		gameObjects = new ArrayList<>();
 		buildGameObjects();
 		GameBoard.getGameBoard(gameObjects);
 	}
-	public void buildGameObjects(){
-		gameObjects.add(GameObjectFactory.getGameObject("player", 1,8));
-		gameObjects.add(GameObjectFactory.getGameObject("obstacle", 2,1));
-		gameObjects.add(GameObjectFactory.getGameObject("obstacle", 4,3));
-		gameObjects.add(GameObjectFactory.getGameObject("obstacle", 5,8));
-		gameObjects.add(GameObjectFactory.getGameObject("obstacle", 6,8));
-		gameObjects.add(GameObjectFactory.getGameObject("obstacle", 6,1));
-		gameObjects.add(GameObjectFactory.getGameObject("obstacle", 7,1));
-		gameObjects.add(GameObjectFactory.getGameObject("enemy", 2,2));
-		gameObjects.add(GameObjectFactory.getGameObject("enemy", 3,5));
-		gameObjects.add(GameObjectFactory.getGameObject("enemy", 8,3));
-		gameObjects.add(GameObjectFactory.getGameObject("exit", 7,3));
-		
+
+	public void buildGameObjects()
+	{
+		//List of GameObjects to be placed on the board
+		String[] objectModels = {"player","obstacle","obstacle","obstacle","obstacle","obstacle","obstacle","enemy","enemy","enemy","exit"};
+		int[] xCoords = {1,2,4,5,6,6,7,2,3,8,7};
+		int[] yCoords = {8,1,3,8,8,1,1,2,5,3,3};
+
+		for(int i=0; i<objectModels.length; i++) {
+			try {
+				gameObjects.add(GameObjectFactory.getGameObject(objectModels[i], xCoords[i], yCoords[i]));
+			} catch (InvalidObjectException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
